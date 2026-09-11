280de3c4d6010b47f4977f69a34ef0d726ee235d
I'mDuvalinBS <jg0455748@gmail.com>
2026-09-11 04:45:08 -0600

    Refactor Navbar to include ProfileMenu and showMenu state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,18 @@
+import { useState } from 'react';
 import { useApp } from '../context/AppContext';
+import ProfileMenu from './ProfileMenu';
 
-export default function Navbar() {
-  const { user, logout } = useApp();
+export default function Navbar({ setView }) {
+  const { user } = useApp();
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
     <nav className="navbar">
       <span className="navbar-brand">Resonancia</span>
-      <div className="navbar-user">
-        <span>{user.name}</span>
-        <button className="btn-logout" onClick={logout}>Salir</button>
-      </div>
+      <button className="navbar-avatar" onClick={() => setShowMenu(true)}>
+        {user.name.charAt(0).toUpperCase()}
+      </button>
+      {showMenu && <ProfileMenu setView={setView} onClose={() => setShowMenu(false)} />}
     </nav>
   );
 }
